4d585b0b52aa6b9e9f9a0967288da125c2b1ee45
Евгений <redov64@gmail.com>
2022-03-04 21:00:46 +0800

    Обратная сторона реальности(fix)

--- project/src/components/film-card/film-card.tsx
+++ project/src/components/film-card/film-card.tsx
@@ -17,7 +17,7 @@ function FilmCard({film, onHover, isActive}: FilmCardProps): JSX.Element {
         <img src={previewImage} alt={name} width="280" height="175" />
       </div>
       <h3 className="small-film-card__title">
-        <Link className="small-film-card__link" to={`/film/${id}`}>
+        <Link className="small-film-card__link" to={`/films/${id}`}>
           {name}
         </Link>
       </h3>
